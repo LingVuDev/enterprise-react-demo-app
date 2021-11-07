b420799aba997a555a546bf5863df08171f533f6
Ling Vu <vumanhling@icloud.com>
2021-11-07 21:55:52 +0100

    Remove react feather, set drawer width in the theme and add additional entries to the sidebar

--- src/app/layouts/DashboardLayout/DashboardSidebarNavigation.tsx
+++ src/app/layouts/DashboardLayout/DashboardSidebarNavigation.tsx
@@ -1,35 +1,41 @@
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import {
+  PieChart,
+  ShoppingCart,
+  KeyboardArrowUp,
+  KeyboardArrowDown,
+  List as ListIcon,
+  AddBox,
+  Logout,
+} from '@mui/icons-material';
 import SettingsIcon from '@mui/icons-material/Settings';
+import Collapse from '@mui/material/Collapse';
+import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
 import Toolbar from '@mui/material/Toolbar';
 import classNames from 'classnames';
-import { PieChart as PieChartIcon } from 'react-feather';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useRouteMatch } from 'react-router-dom';
 import styled from 'styled-components';
 
-const drawerWidth = '240px';
-
-const Root = styled.div`
+const RootDrawer = styled(Drawer)`
   display: flex;
+  width: ${({ theme }) => theme.variables.drawerWidth};
+  flex-shrink: 0;
 
-  .drawer {
-    width: ${drawerWidth};
-    flex-shrink: 0;
-
-    & .MuiDrawer-paper {
-      width: ${drawerWidth};
-      box-sizing: border-bo;
-    }
+  & .MuiDrawer-paper {
+    width: ${({ theme }) => theme.variables.drawerWidth};
+    box-sizing: border-bo;
   }
 
   .drawer-paper {
-    width: ${drawerWidth};
+    width: ${({ theme }) => theme.variables.drawerWidth};
   }
 
   .drawer-container {
@@ -57,56 +63,109 @@ const Root = styled.div`
     text-decoration: none;
     color: inherit;
   }
+
+  .nested {
+    padding-left: ${({ theme }) => theme.spacing(4)};
+  }
 `;
 
 const DashboardSidebarNavigation = () => {
   const { url } = useRouteMatch();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(currentOpen => !currentOpen);
+  };
+
   return (
-    <Root>
-      <Drawer
-        className={classNames('drawer', 'drawer-paper')}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar className={classNames('toolbar')}>
-          <Link className={classNames('logo-with-link')} to={url}>
-            Logo
-          </Link>
-        </Toolbar>
-        <List>
-          <Link to={url}>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <PieChartIcon />
-                </ListItemIcon>
-                <ListItemText primary="Dashboard" />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-          <Link to={`${url}/settings-and-privacy`}>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <SettingsIcon />
-                </ListItemIcon>
-                <ListItemText primary="Settings and Privacy" />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-          <a href="/">
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <ExitToAppIcon />
-                </ListItemIcon>
-                <ListItemText primary="Logout" />
-              </ListItemButton>
-            </ListItem>
-          </a>
-        </List>
-      </Drawer>
-    </Root>
+    <RootDrawer
+      className={classNames('drawer-paper')}
+      variant="permanent"
+      anchor="left"
+    >
+      <Toolbar className={classNames('toolbar')}>
+        <Link className={classNames('logo-with-link')} to={url}>
+          Logo
+        </Link>
+      </Toolbar>
+      <List>
+        <ListSubheader>Reports</ListSubheader>
+        {/* Dashboard */}
+        <Link to={url}>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <PieChart />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <ListSubheader>Management</ListSubheader>
+        {/* Products */}
+        <ListItem disablePadding>
+          <ListItemButton onClick={handleOpen}>
+            <ListItemIcon>
+              <ShoppingCart />
+            </ListItemIcon>
+            <ListItemText primary="Products" />
+            {open ? (
+              <KeyboardArrowUp color="primary" />
+            ) : (
+              <KeyboardArrowDown color="primary" />
+            )}
+          </ListItemButton>
+        </ListItem>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            {/* List Products */}
+            <Link to={`${url}/list-products`}>
+              <ListItem className="nested" disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <ListIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="List Products" />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+            {/* Create Product */}
+            <Link to={`${url}/create-product`}>
+              <ListItem className="nested" disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <AddBox />
+                  </ListItemIcon>
+                  <ListItemText primary="Create Product" />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+          </List>
+        </Collapse>
+        {/* Settings and Privacy */}
+        <Link to={`${url}/settings-and-privacy`}>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Settings and Privacy" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        {/* Logout */}
+        <a href="/">
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Logout />
+              </ListItemIcon>
+              <ListItemText primary="Logout" />
+            </ListItemButton>
+          </ListItem>
+        </a>
+      </List>
+    </RootDrawer>
   );
 };
 
--- src/styles/theme/themes.ts
+++ src/styles/theme/themes.ts
@@ -1,6 +1,25 @@
 import { createTheme } from '@mui/material/styles';
 
+declare module '@mui/material/styles' {
+  interface Theme {
+    variables: {
+      [name: string]: string | number;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    variables?: {
+      [name: string]: string | number;
+    };
+  }
+}
+
+const variables = {
+  drawerWidth: '240px',
+};
+
 const lightTheme = createTheme({
+  variables,
   palette: {
     mode: 'light',
     text: {
@@ -11,6 +30,7 @@ const lightTheme = createTheme({
 });
 
 const darkTheme = createTheme({
+  variables,
   palette: {
     mode: 'dark',
     text: {
@@ -19,44 +39,6 @@ const darkTheme = createTheme({
     },
   },
 });
-
-// const lightTheme = {
-//   primary: 'rgba(215,113,88,1)',
-//   text: 'rgba(58,52,51,1)',
-//   textSecondary: 'rgba(58,52,51,0.7)',
-//   background: 'rgba(255,255,255,1)',
-//   backgroundVariant: 'rgba(251,249,249,1)',
-//   border: 'rgba(58,52,51,0.12)',
-//   borderLight: 'rgba(58,52,51,0.05)',
-//   palette: {
-//     type: 'light',
-//     primary: {
-//       main: '#3f51b5',
-//     },
-//     secondary: {
-//       main: '#f50057',
-//     },
-//   },
-// };
-
-// const darkTheme: Theme = {
-//   primary: 'rgba(220,120,95,1)',
-//   text: 'rgba(241,233,231,1)',
-//   textSecondary: 'rgba(241,233,231,0.6)',
-//   background: 'rgba(0,0,0,1)',
-//   backgroundVariant: 'rgba(28,26,26,1)',
-//   border: 'rgba(241,233,231,0.15)',
-//   borderLight: 'rgba(241,233,231,0.05)',
-//   palette: {
-//     type: 'dark',
-//     primary: {
-//       main: '#3f51b5',
-//     },
-//     secondary: {
-//       main: '#f50057',
-//     },
-//   },
-// };
 
 export type Theme = typeof lightTheme;
 
